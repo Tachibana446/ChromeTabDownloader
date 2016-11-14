bbc5ea868a6f1d8df08e0288bf39fe43a09f1e68
Tachibana446 <stinger33z@gmail.com>
2016-11-14 17:01:26 +0900

    popup.jsの方で処理するように
    background.jsの方だと現在のウィンドウを取得できない

--- popup.js
+++ popup.js
@@ -1,13 +1,76 @@
 // debug
 console.log("load popup.js");
 
-var BG = chrome.extension.getBackgroundPage();
+function sleep(second) {
+    var d1 = new Date().getTime();
+    var d2 = new Date().getTime();
+    while (d2 < d1 + 1000 * second) {
+        d2 = new Date().getTime();
+    }
+}
+
+// 文字列から拡張子を取得
+function getExtension(url) {
+    var ret;
+    if (!url) return ret;
+    var splits = url.split(".");
+    var len = splits.length;
+    if (len == 0) return url;
+    ret = splits[len - 1];
+    return ret;
+}
+
+// 文字列が画像拡張子かどうか判別
+function checkPicture(url) {
+    var exts = ["jpg", "jpeg", "png", "bmp", "gif",
+        "JPG", "JPEG", "PNG", "BMP", "GIF"
+    ];
+    var ext = getExtension(url);
+    if (exts.indexOf(ext) >= 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// 現在のウィンドウの画像タブをすべて保存
+function downloadPictures(closeTab) {
+    // tabsプロパティを含むオプション
+    var option = {
+        populate: true
+    };
+    chrome.windows.getCurrent(option, function(window) {
+        var removeTabs = [];
+        for (var tab of window.tabs) {
+            // DEBUG
+            console.table(tab);
+            if (!checkPicture(tab.url)) {
+                continue;
+            }
+            // 名前がかぶったら番号を振る
+            var params = {
+                url: tab.url,
+                conflictAction: chrome.downloads.FilenameConflictAction.uniquify
+            };
+            // DEBUG
+            console.table(params);
+            chrome.downloads.download(params, null);
+            removeTabs.push(tab.id);
+        }
+        sleep(1);
+        if (closeTab) chrome.tabs.remove(removeTabs, null);
+    });
+}
+
+
+//var BG = chrome.extension.getBackgroundPage();
 
 $(function() {
     $("#downloadPictures").click(function() {
-        BG.downloadPictures();
+        downloadPictures(false);
     });
     $("#downloadPictures2").click(function() {
-        BG.downloadPictures(true);
+        console.log("click 2");
+        downloadPictures(true);
     });
 });
